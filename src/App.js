e10fdaac35fef055fb48c99ab882749fa063d15e
Maxime Morize <morizem@awty.org>
2021-03-18 22:10:42 -0500

    Added fetch functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data,setData] = useState([]);
+
+  const getData = () => {
+    fetch("./resources/kanjis_1.json", {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).then((response) => {
+      console.log(response)
+      return response.json();
+    }).then((response) => {
+      console.log(response);
+      setData(response);
+    })
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* { */}
+      {/*   data && data.length > 0 && data.map((item) => { */}
+      {/*     return ( */}
+      {/*       <div> */}
+      {/*         <p>{item.kanjis_kanji}</p> */}
+      {/*         <p>{item.kanjis_kana}</p> */}
+      {/*         <p>{item.kanjis_romaji}</p> */}
+      {/*       </div> */}
+      {/*     )}) */}
+      {/*   } */}
+      <p>{
+        data && data.length > 0 && data[1].kanjis_kanji
+      }</p>
     </div>
   );
 }
