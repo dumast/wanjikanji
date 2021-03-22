0a0d79840dfa5f4415ff50b64ef22c9474f53209
Terence Dumas <watsutx41@@gmail.com>
2021-03-21 19:37:51 -0500

    working for 1st element

--- src/App.js
+++ src/App.js
@@ -13,34 +13,9 @@ import {
 
 function App() {
   //random variables declaration, to select a random radical / kanji / vocab from the whole 60 levels
-  const langtype = ['radicals', 'kanjis', 'vocab']
-  const type = langtype[Math.floor((Math.random()*3))];
-  const num = Math.floor((Math.random()*60)+1);
 
-  //fetch API
-  const [data,setData] = useState([]);
-
-  const getData = (type, num) => {
-    console.log("Lesson: ", num, "type: ", type)
-    fetch(`./resources/${type}_${num}.json`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    }).then((response) => {
-      return response.json();
-    }).then((response) => {
-      setData(response);
-    })
-  }
-
-  useEffect(() => {
-    getData(type, num)
-  }, [])
 
   //random number in list
-  const y = Math.floor((Math.random()*data.length));
-  console.log("Number: ", y+1)
 
   return (
     <div className="App">
@@ -48,12 +23,7 @@ function App() {
         <Switch>
           <Route exact path='/' component = {Home}></Route> 
           <Route path='/Lessons' component = {Lessons}></Route>
-          <Route path='/Card' component = {() => 
-            <Card 
-              kanji = {data && data.length > 0 && data[y].kanji} 
-              kana = {data && data.length > 0 && data[y].kana} 
-              romaji = {data && data.length > 0 && data[y].romaji} 
-            />}>
+          <Route path='/Card' component = {Card}>
           </Route>
           <Route path="/Hub" component = {Hub}></Route>
         </Switch>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
 import Back from './Back';
 import {Link, useLocation} from "react-router-dom";
 
 export default function Card(props){
+
     let location = useLocation();
     const lessonnum = location.state.lessonnum.lessonnum
     const type = location.state.family
+
+    const [data,setData] = useState([]);
+
+    const getData = (type, lessonnum) => {
+    console.log("Lesson: ", lessonnum, "type: ", type)
+    fetch(`./resources/${type}_${lessonnum}.json`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((response) => {
+      setData(response);
+    })
+    }
+
+    useEffect(() => {
+        getData(type, lessonnum)
+    }, [])
+
+    const y = 0
+
+    const kanji = data && data.length > 0 && data[y].kanji
+    const kana = data && data.length > 0 && data[y].kana
+    const romaji = data && data.length > 0 && data[y].romaji
+
     return(
         <div className = "card">
         <p className = "nav">Wanji-Kanji &gt; Lessons &gt; Lesson {lessonnum} &gt; {type}</p>
@@ -18,11 +46,11 @@ export default function Card(props){
             >
                 <FrontSide
                     >
-                    <span className = "kanji">{props.kanji}</span>
+                    <span className = "kanji">{kanji}</span>
                 </FrontSide>
                 <BackSide>
-                    <span className = "kanji">{props.kana}</span> 
-                    <span> {props.romaji}</span>
+                    <span className = "kanji">{kana}</span> 
+                    <span> {romaji}</span>
                 </BackSide>
             </Flippy>
             <Back/>
