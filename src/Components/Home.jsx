d6badc35ab3906bc23f898e106522804d77db3fc
Terence Dumas <watsutx41@@gmail.com>
2021-03-23 18:43:33 -0500

    wanikani.com

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,6 +8,7 @@ export default function Home() {
         <div>
             <h1>Wanji-Kanji</h1>
                 <Link to = "/Lessons"><button className = 'lessons'>Lessons</button></Link>
+            <h6 className = "wanikani"><a href = "https://wanikani.com" target="_blank">WaniKani.com</a></h6>
         </div>
     )
 }
